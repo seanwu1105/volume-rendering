c2137180624fa21c79ed031a1013fec702747ca8
Shuang Wu <seanwu1105@gmail.com>
2023-02-18 12:27:26 -0500

    feat: use partial magma color scales for flame isosurfaces

--- iso_flame.py
+++ iso_flame.py
@@ -6,11 +6,10 @@ if __name__ == "__main__":
     import_for_rendering_core()
     args = parse_args()
 
-    # TODO: Update these configs to match the data
     configs: tuple[IsosurfaceConfig, ...] = (
-        {"isovalue": 631, "color": (0.898, 0.7098, 0.631, 0.3)},
-        {"isovalue": 1029, "color": (0.67, 0.21, 0.21, 0.3)},
-        {"isovalue": 1226, "color": (0.898, 0.898, 0.898, 0.4)},
+        {"isovalue": 6500, "color": (0.372116, 0.092816, 0.499053, 0.8)},
+        {"isovalue": 30000, "color": (0.828886, 0.262229, 0.430644, 0.8)},
+        {"isovalue": 65000, "color": (0.997341, 0.733545, 0.505167, 0.7)},
     )
 
     render_isosurfaces(args.input, configs).Start()
